d68bf2cc7be0c8193dc06de6c173e03f5df2ec78
Esraa Ehab <esraa.ehab105@gmail.com>
2026-05-30 20:10:18 +0300

    add dbt model (stg_diagnosis)

---
+++ raqib_sf/models/staging/stg_diagnosis.sql
@@ -0,0 +1,20 @@
+with raw_diagnosis as (
+    select * 
+    from {{ source('raw', 'raw_diagnosis') }}
+)
+
+select 
+    diagnosis_id,
+    patient_id,
+    admission_id,
+    provider_id,
+    admission_id,
+    provider_id,
+    TRIM(REGEXP_REPLACE(description, '\\s*\\([^)]+\\)', '')) as description,
+    REGEXP_SUBSTR(description, '\\(([^)]+)\\)', 1, 1, 'e', 1) AS description_type,
+    TRIM(SPLIT_PART(REGEXP_REPLACE(specialty, '\\s*/\\s*', ' & '), ' & ', 1)) AS specialty_1,
+    NULLIF(TRIM(SPLIT_PART(REGEXP_REPLACE(specialty, '\\s*/\\s*', ' & '), ' & ', 2)), '') AS specialty_2,
+    TRIM(SPLIT_PART(REGEXP_REPLACE(SPLIT_PART(sub_domain, ' – ', 2), '\\s*/\\s*', ' & '), ' & ', 1)) AS sub_domain_1,
+    NULLIF(TRIM(SPLIT_PART(REGEXP_REPLACE(SPLIT_PART(sub_domain, ' – ', 2), '\\s*/\\s*', ' & '), ' & ', 2)), '') AS sub_domain_2,
+    sub_domain_key
+from raw_diagnosis
