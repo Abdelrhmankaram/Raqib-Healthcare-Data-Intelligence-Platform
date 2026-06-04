a5f2aaed330a782b960ea78139aad673a6b3c90a
samawael7 <samawael764@gmail.com>
2026-06-04 21:38:14 +0300

    Finalizing Raqib Data Models

--- raqib_sf_dbt/models/marts/dim/dim_patients.sql
+++ raqib_sf_dbt/models/marts/dim/dim_patients.sql
@@ -1,23 +1,27 @@
-with patients as (
-    select
-        *
-    from {{ ref('stg_patients') }}
+WITH patients AS (
+    SELECT * FROM {{ ref('stg_patients') }}
+),
+emergency_contacts AS (
+    SELECT * FROM {{ ref('stg_emergency_contacts') }}
 )
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['patient_id']) }} AS patient_key,
-    patient_id,
-    ssn,
-    full_name,
-    birthdate,
-    birth_year,
-    deathdate,
-    is_dead,
-    blood_type,
-    marital_status,
-    Race,
-    ethnicity,
-    spoken_language,
-    city
-    
-FROM patients
+    {{ dbt_utils.generate_surrogate_key(['p.patient_id']) }} AS patient_key,
+    p.patient_id,
+    p.ssn,
+    p.full_name,
+    p.birthdate,
+    p.birth_year,
+    p.deathdate,
+    p.is_dead,
+    p.blood_type,
+    p.marital_status,
+    p.Race,
+    p.ethnicity,
+    p.spoken_language,
+    p.city,
+    ec.emergency_contact_name,
+    ec.emergency_contact_phone_number
+FROM patients p
+LEFT JOIN emergency_contacts ec 
+    ON p.patient_id = ec.patient_id
